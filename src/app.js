64bafd67b3c38b9ffcbb0b818ec3f9a6a28a03dc
darshAnup24 <150640517+darshAnup24@users.noreply.github.com>
2026-03-09 19:34:28 +0530

    Update greeting message in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' hello banglore  ');
+  res.send(' hello banglorre 2  ');
 });
 
 app.get('/health', (req, res) => {
